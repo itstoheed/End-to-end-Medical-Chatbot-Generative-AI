e9eb202b1a135e9ee7f4486b770fa4488e86c2d8
itstoheed <tohee66@gmail.com>
2024-12-17 23:31:30 +0500

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Muhammad Tauheed Hussain',
+    author_email= 'tohee66@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
